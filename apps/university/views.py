2fdd2a5dd0957602b81634f751ffc8ffbd05ed17
BoastfulApex <jahongirnormuminov8@gmail.com>
2023-06-14 11:42:35 +0500

    auth model edited

--- apps/university/views.py
+++ apps/university/views.py
@@ -4,17 +4,17 @@ from .models import StudyType, Faculty, FacultyType
 from .serializers import StudyTypeSerializer, FacultySerializer, FacultyTypeSerializer
 
 
-class StudyTypeListView(generics.ListAPIView):
+class StudyTypeListView(generics.ListCreateAPIView):
     queryset = StudyType.objects.all()
     serializer_class = StudyTypeSerializer
 
 
-class FacultyListView(generics.ListAPIView):
+class FacultyListView(generics.ListCreateAPIView):
     queryset = Faculty.objects.all()
     serializer_class = FacultySerializer
 
 
-class FacultyTypeListView(generics.ListAPIView):
+class FacultyTypeListView(generics.ListCreateAPIView):
     serializer_class = FacultyTypeSerializer
 
     def get_queryset(self):
