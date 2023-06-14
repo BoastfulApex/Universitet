16d0f1cb5986f079778f1c4da3b9e4335a0563a5
BoastfulApex <jahongirnormuminov8@gmail.com>
2023-06-14 11:47:39 +0500

    auth model edited

--- apps/users/views.py
+++ apps/users/views.py
@@ -75,21 +75,21 @@ class UserDataPostView(generics.CreateAPIView):
     permission_classes = [permissions.IsAuthenticated]
 
     def post(self, request, *args, **kwargs):
-        # try:
+        try:
             user = request.user
             data = request.data
             user.passport_seria = data['passport_seria']
             user.full_name = data['full_name']
             user.date_if_birth = data['date_if_birth']
-            # user.diploma_picture = data['diploma_picture']
-            # user.ielts_picture = data['ielts_picture']
-            # study_type = StudyType.objecys.get(id=data['study_type'])
-            # user.study_type = study_type
-            # faculty = Faculty.objecys.get(id=data['faculty'])
-            # user.faculty = faculty
-            # faculty_type = FacultyType.objecys.get(id=data['faculty_type'])
-            # user.type = faculty_type
-            # user.save()
+            user.diploma_picture = data['diploma_picture']
+            user.ielts_picture = data['ielts_picture']
+            study_type = StudyType.objecys.get(id=data['study_type'])
+            user.study_type = study_type
+            faculty = Faculty.objecys.get(id=data['faculty'])
+            user.faculty = faculty
+            faculty_type = FacultyType.objecys.get(id=data['faculty_type'])
+            user.type = faculty_type
+            user.save()
             return Response({'status': 'created'})
-        # except Exception as exx:
-        #     return Response({"error": str(exx)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+        except Exception as exx:
+            return Response({"error": str(exx)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
