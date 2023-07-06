f8b5b9d7687b709d523c13fa2934dfa18865799a
BoastfulApex <jahongirnormuminov8@gmail.com>
2023-07-06 21:42:40 +0500

    moderator permissions edited

--- apps/moderator/permission_classes.py
+++ apps/moderator/permission_classes.py
@@ -5,68 +5,75 @@ from rest_framework.exceptions import AuthenticationFailed
 class CreateGroupFacultyType(BasePermission):
 
     def has_permission(self, request, view):
-        if not request.user.is_authenticated:
-            raise AuthenticationFailed(detail='Foydalanuvchi uchun ruxsat mavjud emas!!!', code=401)
-        elif not request.user.create_group_faculty_type or not request.user.super_admin:
-            raise AuthenticationFailed(detail='Foydalanuvchi uchun ruxsat mavjud emas!!!', code=401)
-        return True
+        if request.user.is_authenticated:
+            if request.user.create_group_faculty_type or request.user.super_admin:
+                return True
+            else:
+                return False
+        return False
 
 
 class CreateSubjectTest(BasePermission):
 
     def has_permission(self, request, view):
-        if not request.user.is_authenticated:
-            raise AuthenticationFailed(detail='Foydalanuvchi uchun ruxsat mavjud emas!!!', code=401)
-        elif not request.user.create_subject  or not request.user.super_admin:
-            raise AuthenticationFailed(detail='Foydalanuvchi uchun ruxsat mavjud emas!!!', code=401)
-        return True
+        if request.user.is_authenticated:
+            if request.user.create_subject or request.user.super_admin:
+                return True
+            else:
+                return False
+        return False
 
 
 class WorkingStudent(BasePermission):
 
     def has_permission(self, request, view):
-        if not request.user.is_authenticated:
-            raise AuthenticationFailed(detail='Foydalanuvchi uchun ruxsat mavjud emas!!!', code=401)
-        elif not request.user.working_with_student or not request.user.super_admin:
-            raise AuthenticationFailed(detail='Foydalanuvchi uchun ruxsat mavjud emas!!!', code=401)
-        return True
+        if request.user.is_authenticated:
+            if request.user.working_with_student or request.user.super_admin:
+                return True
+            else:
+                return False
+        return False
 
 
 class WorkingApplicant(BasePermission):
 
     def has_permission(self, request, view):
-        if not request.user.is_authenticated:
-            raise AuthenticationFailed(detail='Foydalanuvchi uchun ruxsat mavjud emas!!!', code=401)
-        elif request.user.working_with_applicant or not request.user.super_admin:
-            raise AuthenticationFailed(detail='Foydalanuvchi uchun ruxsat mavjud emas!!!', code=401)
-        return True
+        if request.user.is_authenticated:
+            if request.user.working_with_applicant or request.user.super_admin:
+                return True
+            else:
+                return False
+        return False
 
 
 class SendMessage(BasePermission):
 
     def has_permission(self, request, view):
-        if not request.user.is_authenticated:
-            raise AuthenticationFailed(detail='Foydalanuvchi uchun ruxsat mavjud emas!!!', code=401)
-        elif not request.user.send_message or not request.user.super_admin:
-            raise AuthenticationFailed(detail='Foydalanuvchi uchun ruxsat mavjud emas!!!', code=401)
-        return True
+        if request.user.is_authenticated:
+            if request.user.send_message or request.user.super_admin:
+                return True
+            else:
+                return False
+        return False
 
 
 class EditGroup(BasePermission):
 
     def has_permission(self, request, view):
-        if not request.user.is_authenticated:
-            raise AuthenticationFailed(detail='Foydalanuvchi uchun ruxsat mavjud emas!!!', code=401)
-        elif not request.user.edit_group or not request.user.super_admin:
-            raise AuthenticationFailed(detail='Foydalanuvchi uchun ruxsat mavjud emas!!!', code=401)
-        return True
+        if request.user.is_authenticated:
+            if request.user.edit_group or request.user.super_admin:
+                return True
+            else:
+                return False
+        return False
 
 
 class Finance(BasePermission):
 
     def has_permission(self, request, view):
-        if not request.user.is_authenticated:
-            raise AuthenticationFailed(detail='Foydalanuvchi uchun ruxsat mavjud emas!!!', code=401)
-        elif not request.user.finance or not request.user.super_admin:
-            raise AuthenticationFailed(detail='Foydalanuvchi uchun ruxsat mavjud emas!!!', code=401)
-        return True
+        if request.user.finance:
+            if request.user.working_with_applicant or request.user.super_admin:
+                return True
+            else:
+                return False
+        return False
